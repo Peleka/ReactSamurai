9a90e9bbfcfaee619693be824d4cb6025af84b50
Peleka <ego.iriska@gmail.com>
2021-05-01 16:36:44 +0300

    fix UncontrolledRating

--- src/components/UncontrolledRaiting/UncontrolledRating.stories.tsx
+++ src/components/UncontrolledRaiting/UncontrolledRating.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UncontrolledRating} from "./UncontrolledRating";
+import {action} from "@storybook/addon-actions";
 
 
 
@@ -7,10 +8,13 @@ export default { //экспортируем название какую комп
     title: 'UncontrolledRating ',
     component: UncontrolledRating,
 };
+
+const callback = action("rating changed inside component")
+
 //экспортируем отдельные истории
-export const EmptyRating = () => <UncontrolledRating defaultValue={0} />;
-export const Rating1 = () => <UncontrolledRating defaultValue={1} />;
-export const Rating2 = () => <UncontrolledRating defaultValue={2} />;
-export const Rating3 = () => <UncontrolledRating defaultValue={3} />;
-export const Rating4 = () => <UncontrolledRating defaultValue={4} />;
-export const Rating5 = () => <UncontrolledRating defaultValue={5} />;
+export const EmptyRating = () => <UncontrolledRating defaultValue={0} onChange={callback}/>;
+export const Rating1 = () => <UncontrolledRating defaultValue={1}  onChange={callback}/>;
+export const Rating2 = () => <UncontrolledRating defaultValue={2}  onChange={callback}/>;
+export const Rating3 = () => <UncontrolledRating defaultValue={3}  onChange={callback}/>;
+export const Rating4 = () => <UncontrolledRating defaultValue={4}  onChange={callback}/>;
+export const Rating5 = () => <UncontrolledRating defaultValue={5}  onChange={callback}/>;
