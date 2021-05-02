45800e700545d2755cf781e64fc8f1df03afabde
Peleka <ego.iriska@gmail.com>
2021-05-02 21:48:59 +0300

    storybook doc

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,20 +1,60 @@
 import React, {useState} from 'react';
-import { action } from '@storybook/addon-actions';
-import {Accordion} from "./Accordion";
+import {action} from '@storybook/addon-actions';
+import {Accordion, AccordionPropsType} from "./Accordion";
+import {Story} from '@storybook/react';
 
-
+const CategoryObj = (categoryName: string) => ({
+    table: {
+        category: categoryName
+    }
+})
 
 export default {
-    title: 'Accordion',
+    title: 'components/Accordion',
     component: Accordion,
+    argType: {
+        color: {
+            control: "color",
+            ...CategoryObj('Color')
+        },
+        onChange: {
+            ...CategoryObj('Events')
+        }
+    }
 };
 
 const callback = action('accordion mode change event fired')
 
-export const CollapsedMode = () => <Accordion titleValue={'Wish'} onChange={callback} collapsed={true}/>
-export const UnCollapsedMode = () => <Accordion titleValue={'Users'} onChange={callback} collapsed={false}/>
+const Template: Story<AccordionPropsType> = (args) => <Accordion {...args} />;
+const callbackProps = {
+    onChange: callback
+}
 
-export const ModeChanging = () => {
-    const[value, setValue] = useState<boolean>(false)
-    return <Accordion titleValue={'Users'} onChange={() => setValue(!value)} collapsed={value}/>
+export const CollapsedMode = Template.bind({})
+CollapsedMode.args = {
+    ...callbackProps,
+    titleValue: 'Wish',
+    /**
+     * тут можно написать комментарий ctr+Q подсказка
+     */
+    collapsed: true
+}
+
+export const UnCollapsedMode = Template.bind({})
+UnCollapsedMode.args = {
+    ...callbackProps,
+    titleValue: 'Users',
+    collapsed: false
+}
+//
+// export const CollapsedMode = () => <Accordion titleValue={'Wish'} onChange={callback} collapsed={true}/>
+// export const UnCollapsedMode = () => <Accordion titleValue={'Users'} onChange={callback} collapsed={false}/>
+
+export const ModeChanging: Story<AccordionPropsType> = (args) => {
+    const [value, setValue] = useState<boolean>(false)
+    // return <Accordion titleValue={'Users'} onChange={() => setValue(!value)} collapsed={value}/>
+    return <Accordion {...args} onChange={ () => setValue(!value)} collapsed={value}/>
+}
+ModeChanging.args = {
+    titleValue: 'Users'
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,16 +1,23 @@
 import React, {useState} from "react";
 
-type AccordionPropsType = {
+export type AccordionPropsType = {
     titleValue: string
     onChange: () => void
     collapsed: boolean
+    /**
+     * optional color header text
+     */
+    color: string
 }
 
 export function Accordion(props: AccordionPropsType) {
 
     return (
         <div>
-            <AccordionTitle title={props.titleValue} onClick={props.onChange}/>
+            <AccordionTitle title={props.titleValue}
+                            color={props.color}
+
+                            onClick={props.onChange}/>
             {!props.collapsed && <AccordionBody/>}
         </div>
     )
@@ -19,12 +26,15 @@ export function Accordion(props: AccordionPropsType) {
 type AccordionTitlePropsType = {
     onClick: () => void
     title: string
+    color?: string
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     return (
         <>
-            <h3 onClick={(e) => props.onClick()}>--{props.title}--</h3>
+            <h3
+                style={{color: props.color ? props.color : "black"}}
+                onClick={(e) => props.onClick()}>--{props.title}--</h3>
         </>
     )
 }
